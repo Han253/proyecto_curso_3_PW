6ac1ff297b0288ba0971b5ad944229029be05ea5
han253 <han253@gmail.com>
2024-11-25 14:32:18 -0500

    add models and views and serializadores

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Carro
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serial import CarroSerializer
 
-# Create your views here.
+#Conjunto de vistas para el CRUD del modelo ejemplo
+class CarroViewSet(viewsets.ModelViewSet):
+    """
+    API-REST endpoint para el modelo carro, admite
+    GET,POST, PUT, PATCH, DELETE
+    """
+    queryset = Carro.objects.all().order_by('-fecha_creacion')
+    serializer_class = CarroSerializer
--- back_3/urls.py
+++ back_3/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from rest_framework import routers
+
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'carro', viewset=views.CarroViewSet)
+
 
 urlpatterns = [
+    path('',include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
